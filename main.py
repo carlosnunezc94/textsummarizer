4c25f433ed8ff9c58f10aefa63eae6d590ae2cbf
Carlos Nunez <carlos_nunez@berkeley.edu>
2023-05-29 18:28:37 -0700

    data_ingestion notebook added and modular

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from textsummarizer .logging import logger
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "data_ingestion"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
