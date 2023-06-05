a4588411ea3554834c2f7430bcf52f440f6bf9a2
Carlos Nunez <carlos_nunez@berkeley.edu>
2023-06-04 23:07:03 -0700

    Model training modular completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textsummarizer.logging import logger
 
 
@@ -31,6 +32,17 @@ try:
     logger.info(f"Stage {STAGE_NAME} started")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "model_trainer"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
