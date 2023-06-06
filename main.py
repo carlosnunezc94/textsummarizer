f2a5eee2cd0fc0b1d3d5f81f9d5109e59152e0fd
Carlos Nunez <carlos_nunez@berkeley.edu>
2023-06-05 21:54:15 -0700

    Model Evaluation Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textsummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textsummarizer.logging import logger
 
 
@@ -43,6 +44,17 @@ try:
     logger.info(f"Stage {STAGE_NAME} started")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "model_evaluation"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
